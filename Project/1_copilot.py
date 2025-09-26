31dc57e1da4d26fdd69578785aaabd4d91366ea9
nickcinnamo <nicholascinnamo@gmail.com>
2025-09-26 19:22:27 +0000

    Task C

--- Project/1_copilot.py
+++ Project/1_copilot.py
@@ -1,8 +1,12 @@
 # list of three students named Jon, Kim and Lee
 students = ['Jon', 'Kim', 'Lee']
+# add Sara and Miko to the list
+students.append('Sara')
+students.append('Miko')
 # function to print ‘Hi name’ for each student in the list
 def greet_students(student_list):
     for student in student_list:
         print(f'Hi {student}')
+    print(f'Total number of students: {len(student_list)}')
 # call the function
 greet_students(students)
